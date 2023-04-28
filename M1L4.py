24ed1fda8ae1f318c3560357635036b719ba67d8
AZER <129597661+Azer05042012@users.noreply.github.com>
2023-04-28 22:00:50 +0400

    Add files via upload

---
+++ M1L4.py
@@ -0,0 +1,22 @@
+import discord
+from discord.ext import commands
+import random
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Привет! Я бот {bot.user}')
+
+@bot.command()
+async def number(ctx, *choices: '0, 1, 2, 3, 4, 5, 6, 7, 8, 9'):
+    await ctx.send(random.choice(choices))
+
+bot.run("MTA5NzkzNzA4Njk1MjI2Mzg0Mg.GkA6hY.Uc4SAIyn0g8MRwe_mniSTWi00btpC1J9D7BfuI") 
